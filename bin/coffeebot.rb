a9f81a31a365a6eb9a733cd100a2e68b9b4c7618
Vinoth kumar A <avinoth@users.noreply.github.com>
2015-10-14 21:02:24 +0530

    Dont post messages on weekends

--- bin/coffeebot.rb
+++ bin/coffeebot.rb
@@ -1,11 +1,15 @@
 require 'httparty'
 require 'json'
+requrie 'date'
 
 def post_message author, quote
   img = ENV["IMAGE_URL"] || "https://libcom.org/files/images/library/fist.jpg"
   HTTParty.post SLACK_WEBHOOK, body: {"text" => quote, "username" => author, "icon_url" => img}.to_json, headers: {'content-type' => 'application/json'}
 end
 
-SLACK_WEBHOOK = ENV["SLACK_WEBHOOK"]
 
-post_message(ENV["BOT_NAME"], ENV["MESSAGE"])
+unless ['Saturday', 'Sunday'].include? Date.today.strftime('%A')
+  SLACK_WEBHOOK = ENV["SLACK_WEBHOOK"]
+  
+  post_message(ENV["BOT_NAME"], ENV["MESSAGE"])
+end
